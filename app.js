4d63e996e6550dec0e4b7d2664e93d806120af3d
burnee <burenbat.a@gmail.com>
2023-07-21 18:49:46 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -10,20 +10,21 @@ var roundScore = 0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 var dice = Math.floor(Math.random() * 6) + 1;
 
-//window.document.querySelector("#score-0").textContent = dice;
-//document.querySelector("#score-1").innerHTML = '<em>Yes!</em>';
-
 //Програм эхлэхэд бэлтгэе
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
 
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
 
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
 
+    //alert("Шоо буулаа : " + diceNumber);
+});
 
-
-
-console.log('Шоо : ' + dice);
